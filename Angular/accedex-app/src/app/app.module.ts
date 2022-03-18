60e60b4821a4a297ac221e8e11083170fb126244
Victor Monzo Mora <victor.monzo.mora@gmail.com>
2022-03-18 11:05:35 +0100

    Add routing + principals components

--- Angular/accedex-app/src/app/app.module.ts
+++ Angular/accedex-app/src/app/app.module.ts
@@ -1,14 +1,30 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'pokemon', component: PokemonComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PokemonComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
