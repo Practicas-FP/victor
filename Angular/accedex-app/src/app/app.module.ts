2937bf169eaad3f005b9c7490f2d9f3b557ca4ef
Victor Monzo Mora <victor.monzo.mora@gmail.com>
2022-03-18 11:34:59 +0100

    Add navbar + component pokemon

--- Angular/accedex-app/src/app/app.module.ts
+++ Angular/accedex-app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -7,9 +7,11 @@ import { HomeComponent } from './home/home.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { FormsModule } from '@angular/forms';
+import { PokedexComponent } from './pokedex/pokedex.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'pokedex', component: PokedexComponent },
   { path: 'pokemon', component: PokemonComponent },
   { path: '**', component: NotFoundComponent }
 ];
@@ -19,7 +21,8 @@ const routes: Routes = [
     AppComponent,
     HomeComponent,
     PokemonComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    PokedexComponent
   ],
   imports: [
     BrowserModule,
