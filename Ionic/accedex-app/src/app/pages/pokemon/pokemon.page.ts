e3bb16a0f0d93063beee47e9db85e53d6555fa36
Victor Monzo Mora <victor.monzo.mora@gmail.com>
2022-03-30 21:06:50 +0200

    refactor code

--- Ionic/accedex-app/src/app/pages/pokemon/pokemon.page.ts
+++ Ionic/accedex-app/src/app/pages/pokemon/pokemon.page.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/member-ordering */
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { LoadingController, ToastController } from '@ionic/angular';
-import { Chart } from 'chart.js';
+import { ToastController } from '@ionic/angular';
 import { PokeapiService } from 'src/app/services/pokeapi.service';
 
 @Component({
@@ -52,50 +51,5 @@ export class PokemonPage implements OnInit {
     toast.present();
   }
 
-  /*// eslint-disable-next-line @angular-eslint/use-lifecycle-interface
-  ngAfterViewInit() {
-    this.barChartMethod();
-  }
 
-  @ViewChild('barCanvas') private barCanvas: ElementRef;
-  barChart: any;
-
-  barChartMethod() {
-    this.barChart = new Chart(this.barCanvas.nativeElement, {
-      type: 'bar',
-      data: {
-        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
-        datasets: [{
-          label: '# of Votes',
-          data: [200, 50, 30, 15, 20, 34],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      /* options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
-  } */
 }
