181a52d41f669f56f753fa28396d33f2734af01c
Victor Monzo Mora <victor.monzo.mora@gmail.com>
2022-03-31 09:05:14 +0200

    refactor code

--- Ionic/accedex-app/src/app/pages/pokemon/pokemon.page.ts
+++ Ionic/accedex-app/src/app/pages/pokemon/pokemon.page.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/member-ordering */
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import { Chart } from 'chart.js';
 import { PokeapiService } from 'src/app/services/pokeapi.service';
 
 @Component({
@@ -9,7 +10,7 @@ import { PokeapiService } from 'src/app/services/pokeapi.service';
   templateUrl: './pokemon.page.html',
   styleUrls: ['./pokemon.page.scss'],
 })
-export class PokemonPage implements OnInit {
+export class PokemonPage implements OnInit/*, AfterViewInit*/ {
 
   private id: number;
 
@@ -52,4 +53,48 @@ export class PokemonPage implements OnInit {
   }
 
 
+  // Grafico stats
+  /*@ViewChild('barCanvas') private barCanvas: ElementRef;
+  barChart: any;
+
+  ionViewWillEnter() {
+    this.barChartMethod();
+
+  }
+
+  ngAfterViewInit() {
+    //this.barChartMethod();
+  }
+
+  barChartMethod() {
+    setTimeout(() => {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
+        datasets: [{
+          label: '# of Votes',
+          data: [200, 50, 30, 15, 20, 34],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      }
+    });
+  }, 1000);
+  }*/
 }
