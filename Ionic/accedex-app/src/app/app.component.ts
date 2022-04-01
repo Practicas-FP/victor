6e042b77ac735fc20ba9ed62b823fc71c8cf330d
Victor Monzo Mora <victor.monzo.mora@gmail.com>
2022-04-01 14:52:52 +0200

    toggle DM

--- Ionic/accedex-app/src/app/app.component.ts
+++ Ionic/accedex-app/src/app/app.component.ts
@@ -14,9 +14,26 @@ export class AppComponent {
     { title: 'Profile', url: 'profile', icon: 'person' }
   ];
 
-  constructor(public authService: AuthService, public router: Router) { }
+  public darkMode: boolean;
+  private darkModeSt = 'darkMode';
+
+  constructor(public authService: AuthService, public router: Router) {
+    if (localStorage.getItem(this.darkModeSt)) {
+      this.darkMode = Boolean(localStorage.getItem(this.darkModeSt));
+
+      console.log(`Al cargar: ${localStorage.getItem(this.darkModeSt)}`);
+    }
+  }
 
   findPokemonByName(name: string) {
     this.router.navigate([`pokemon/` + name]);
+  }
+
+  changeMode(value: string) {
+    console.log(`Antes de guardar: ${value}`);
+
+    localStorage.setItem(this.darkModeSt, this.darkMode ? '1' : '0');
+
+    console.log(localStorage.getItem(this.darkModeSt));
   }
 }
