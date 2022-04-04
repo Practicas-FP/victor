4ed2b78e84af5cd07b10b588cdcebf5796e5ccc4
Victor Monzo Mora <victor.monzo.mora@gmail.com>
2022-04-04 08:21:20 +0200

    add prod angular

--- Angular/accedex-app/src/environments/environment.prod.ts
+++ Angular/accedex-app/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBGeBCGwQIhCv1J-qjY329NbEE8Tl5B-2Q",
+    authDomain: "accedex-decb9.firebaseapp.com",
+    databaseURL: "https://accedex-decb9-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "accedex-decb9",
+    storageBucket: "accedex-decb9.appspot.com",
+    messagingSenderId: "552211236830",
+    appId: "1:552211236830:web:c7ad8bf411bc007e603f4f",
+    measurementId: "G-P2G6WCSEST"
+  }
 };
