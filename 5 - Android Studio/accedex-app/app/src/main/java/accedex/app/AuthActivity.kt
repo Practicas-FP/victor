69bd35e4d1eb5d8a717f63e6c9f6abcb1719b441
Victor Monzo Mora <victor.monzo.mora@gmail.com>
2022-05-10 08:34:33 +0200

    fix errors login

--- 5 - Android Studio/accedex-app/app/src/main/java/accedex/app/AuthActivity.kt
+++ 5 - Android Studio/accedex-app/app/src/main/java/accedex/app/AuthActivity.kt
@@ -3,7 +3,6 @@ package accedex.app
 import accedex.app.Constants.Companion.DISPLAY_NAME
 import accedex.app.Constants.Companion.IS_EMAIL_VERIFIED
 import accedex.app.Constants.Companion.PHOTO_URL
-import accedex.app.Constants.Companion.TAG
 import accedex.app.Constants.Companion.UID
 import accedex.app.databinding.ActivityAuthBinding
 import accedex.app.jk.ProviderType
@@ -12,7 +11,6 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -53,50 +51,62 @@ class AuthActivity : AppCompatActivity() {
     private fun setUI() {
         binding.btLogin.setOnClickListener {
             if (binding.etEmailRegister.text.isNotBlank() && binding.etPasswordRegister.text.isNotEmpty()) {
-                FirebaseAuth.getInstance().signInWithEmailAndPassword(
-                    binding.etEmailRegister.text.toString(),
-                    binding.etPasswordRegister.text.toString()
-                ).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        loginSuccessful(constants.createUser(it, ProviderType.BASIC, null))
-                    } else {
-                        constants.showError(
-                            this,
-                            getString(R.string.login_auth_error)
-                        )
+                if (constants.checkForInternet(this)) {
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                        binding.etEmailRegister.text.toString(),
+                        binding.etPasswordRegister.text.toString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            loginSuccessful(constants.createUser(it, ProviderType.BASIC, null))
+                        } else {
+                            constants.showError(
+                                this,
+                                getString(R.string.login_auth_error)
+                            )
+                        }
                     }
+                } else {
+                    constants.showError(this, getString(R.string.no_internet_access))
                 }
             }
         }
 
         binding.btRegister.setOnClickListener {
             if (binding.etEmailRegister.text.isNotBlank() && binding.etPasswordRegister.text.isNotEmpty()) {
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
-                    binding.etEmailRegister.text.toString(),
-                    binding.etPasswordRegister.text.toString()
-                ).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        loginSuccessful(constants.createUser(it, ProviderType.BASIC, null))
-                    } else {
-                        constants.showError(
-                            this,
-                            getString(R.string.register_auth_error)
-                        )
+                if (constants.checkForInternet(this)) {
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                        binding.etEmailRegister.text.toString(),
+                        binding.etPasswordRegister.text.toString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            loginSuccessful(constants.createUser(it, ProviderType.BASIC, null))
+                        } else {
+                            constants.showError(
+                                this,
+                                getString(R.string.register_auth_error)
+                            )
+                        }
                     }
+                } else {
+                    constants.showError(this, getString(R.string.no_internet_access))
                 }
             }
         }
 
         binding.btLoginGoogle.setOnClickListener {
-            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build()
+            if (constants.checkForInternet(this)) {
+                val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build()
 
-            val googleClinet = GoogleSignIn.getClient(this, googleConf)
-            googleClinet.signOut()
+                val googleClinet = GoogleSignIn.getClient(this, googleConf)
+                googleClinet.signOut()
 
-            startActivityForResult(googleClinet.signInIntent, Constants.GOOGLE_SIGN_IN)
+                startActivityForResult(googleClinet.signInIntent, Constants.GOOGLE_SIGN_IN)
+            } else {
+                constants.showError(this, getString(R.string.no_internet_access))
+            }
         }
     }
 
